c76b5743a88b6c15f95eeeddcfc28464c8b27b51
Kongtap Arunlakvilart <35949144+armykongtap@users.noreply.github.com>
2019-09-24 18:51:43 +0700

    test
    just test

--- my-react-app/src/Category.js
+++ my-react-app/src/Category.js
@@ -1,41 +1,30 @@
-import React from 'react';
-import CardDeck from './CardDeck';
-import './Flashcard.css';
-import './button.css';
-
+import React from "react";
+import CardDeck from "./CardDeck";
+import "./Flashcard.css";
+import "./button.css";
 
 class Deck extends React.Component {
   render() {
     return (
-      <div
-        className="Deck"
-        id={"Deck_" + this.props.value}
-      >
-        {'Deck ' + this.props.value}
+      <div className="Deck" id={"Deck_" + this.props.value}>
+        {"Deck " + this.props.value}
       </div>
     );
   }
 }
 
 class Category extends React.Component {
-
   renderDeck(i) {
-    return (
-      <Deck
-        value={i}
-      />
-    )
+    return <Deck value={i} />;
   }
 
-  setDeck(deck){
+  setDeck(deck) {
     CardDeck.setDeck(deck);
   }
 
   render() {
     return (
-      <div
-        className="Category"
-      >
+      <div className="Category">
         {this.renderDeck(1)}
         {this.renderDeck(2)}
         {this.renderDeck(3)}
