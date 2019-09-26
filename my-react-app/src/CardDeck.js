e85d1863a86c092a953d43518dec141b43fd0bcb
Kongtap Arunlakvilart <35949144+armykongtap@users.noreply.github.com>
2019-09-26 12:29:52 +0700

    Update CardDeck.js
    add next,back card button

--- my-react-app/src/CardDeck.js
+++ my-react-app/src/CardDeck.js
@@ -4,53 +4,69 @@ import "./button.css";
 var data = require("./data.json");
 
 class Card extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            current: 0,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 0
+    };
+  }
 
-    render() {
-        return this.props.wordList.length === 0 ? (
-            <div></div>
-        ) : (
-                <div
-                    className={this.props.status}
-                >
-                    {this.props.wordList[this.state.current]}
-                </div>
-            );
-    }
+  render() {
+    return this.props.wordList.length === 0 ? (
+      <div></div>
+    ) : (
+      <div className={this.props.status}>
+        {this.props.wordList[this.state.currentPage]}
+      </div>
+    );
+  }
 }
 
 class CardDeck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCard: 0
+    };
+    this.nextCard = this.nextCard.bind(this);
+    this.backCard = this.backCard.bind(this);
+  }
 
-    getWordList(wordID) {
-        return this.props.deckID === null ?
-            null :
-            data[this.props.deckID][wordID];
-    }
+  getWordList(wordID) {
+    return this.props.deckID === null ? null : data[this.props.deckID][wordID];
+  }
 
-    renderCard(wordID) {
-        return (
-            this.getWordList(wordID) === null ?
-                null :
-                <Card
-                    wordList={this.getWordList(wordID)}
-                    status={"Card"}
-                />
-        );
-    }
+  renderCard(wordID) {
+    return this.getWordList(wordID) === null ? null : (
+      <Card wordList={this.getWordList(wordID)} status={"Card"} />
+    );
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="Card-Deck">{this.renderCard(0)} </div>
-                {/* <button onClick="myFunction()">Click me</button> */}
-            </div>
-        );
+  nextCard() {
+    var tmp = this.state.currentCard + 1;
+    if (tmp < data[this.props.deckID].length) {
+      this.setState({ currentCard: tmp });
     }
+  }
+
+  backCard() {
+    var tmp = this.state.currentCard - 1;
+    if (tmp >= 0) {
+      this.setState({ currentCard: tmp });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="Card-Deck">
+          {this.renderCard(this.state.currentCard)}
+        </div>
+        <button onClick={this.backCard}>Back</button>
+        <button onClick={this.nextCard}>Next</button>
+      </div>
+    );
+  }
 }
 
 export default CardDeck;
